c3979010658215af79a93b642320588b573056d7
siddhantahireofficial <siddhantahireofficial@gmail.com>
2020-09-26 17:52:17 +0530

    image updating is done

--- routes/feed.js
+++ routes/feed.js
@@ -15,4 +15,14 @@ router.post('/post',[
 ],feedController.postFeed);
 
 router.get('/post/:postId',feedController.getPost);
+
+router.put('/post/:postId',[
+    body('title')
+    .trim()
+    .isLength({min:5}),
+    body('content')
+    .trim()
+    .isLength({min:5})
+],feedController.updatePost);
+
 module.exports =router;
