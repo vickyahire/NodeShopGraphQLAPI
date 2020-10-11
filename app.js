afc55f497549b158377e5d8c903e46960a3e7eb0
siddhantahireofficial <siddhantahireofficial@gmail.com>
2020-10-11 22:12:15 +0530

    sign in done using graphql,for error handling use validator

--- app.js
+++ app.js
@@ -33,15 +33,27 @@ app.use('/images',express.static(path.join(__dirname,'images')))
 
 app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE,PATCH');
+    res.setHeader('Access-Control-Allow-Methods','OPTIONS,GET,POST,PUT,DELETE,PATCH');
     res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorization');
+    if(req.method === 'OPTIONS'){
+        return res.sendStatus(200);
+    }
     next();
 });
 
 app.use('/graphql',graphqlHTTP({
     schema:graphqlSchema,
     rootValue:graphqlResolver,
-    graphiql:true
+    graphiql:true,
+    customFormatErrorFn(err){
+        if(!err.originalError){
+            return err;
+        }
+        const data = err.originalError.data;
+        const message = err.message || 'An error occured';
+        const code = err.originalError.code;
+        return{message:message,data:data,code:code};
+    }
 })
 );
 
