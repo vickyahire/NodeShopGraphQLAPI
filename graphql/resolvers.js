e4976d0606d7a903e86b6293be4b35fd7768150f
siddhantahireofficial <siddhantahireofficial@gmail.com>
2020-10-13 22:43:01 +0530

    sign in done using jsonwebtoken authentication

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
 const validator = require('validator');
 const User = require('../models/user');
-
+const jwt = require('jsonwebtoken');
 
 module.exports = {
     createUser: async function({userInput},req){
@@ -32,5 +32,25 @@ module.exports = {
         });
         const createdUser = await user.save();
         return {...createdUser._doc,_id:createdUser._id.toString()}
+    },
+    login: async function({email,password},req) {
+        console.log('hello world')
+        const user = await User.findOne({email:email});
+        if(!user){
+            const error = new Error('User not found');
+            error.code = 401;
+            throw error;
+        }console.log(user)
+        const isEqual = await bcrypt.compare(password,user.password);
+        if(!isEqual){
+            const error = new Error('Password is incorrect');
+            error.code = 401;
+            throw error;
+        }
+        const token = jwt.sign({
+            userId:user._id.toString(),
+            email:user.email
+        },'somesupersecretsecret',{expiresIn:'1h'});
+        return {token:token,userId:user._id.toString()}
     }
 }
