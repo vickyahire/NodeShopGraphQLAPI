358a07510fb8c01beaec3c0a840354e2a13c6b6b
siddhantahireofficial <siddhantahireofficial@gmail.com>
2020-10-21 22:31:15 +0530

    post fetchted

--- graphql/schema.js
+++ graphql/schema.js
@@ -31,8 +31,14 @@ module.exports = buildSchema(`
         userId:String!
     }
 
+    type PostData {
+        posts:[Post!]!
+        totalPosts:Int!
+    }
+
     type RootQuery {
         login(email:String!,password:String!):AuthData!
+        posts:PostData!
     }
 
     input PostInputData {
